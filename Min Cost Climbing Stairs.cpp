e422b783e1782a21c4ab2e4171917a3c6687b10a
Radhika Wadekar <60853138+radhika011@users.noreply.github.com>
2021-10-06 09:49:56 +0530

    Update Min Cost Climbing Stairs.cpp

--- Min Cost Climbing Stairs.cpp
+++ Min Cost Climbing Stairs.cpp
@@ -1,3 +1,4 @@
+//DP Solution
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
@@ -11,5 +12,19 @@ public:
         return min(dp[n-1],dp[n-2]);
         
         
+    }
+};
+//Recursive Solution
+class Solution {
+public:
+    int helper(int i,vector<int> cost){
+        if(i==1||i==0){
+            return cost[i];
+        }
+        return cost[i]+min(helper(i-1,cost),helper(i-2,cost));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        return min(helper(n-2,cost),helper(n-1,cost));
     }
 };
