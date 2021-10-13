9e1a75e48ab4f955e654894e815fdf0078940c00
Radhika Wadekar <60853138+radhika011@users.noreply.github.com>
2021-10-13 10:34:01 +0530

    Create Construct Binary Search Tree from Preorder Traversal.cpp
    Monotonic Stack

---
+++ Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        stack<TreeNode*> st;
+        TreeNode* root = nullptr;
+        
+        TreeNode* parent = nullptr;
+        for(int i =0;i<preorder.size();i++){
+            
+            TreeNode* node = new TreeNode(preorder[i]);
+            if(i==0) root = node;
+            parent = nullptr;
+            
+            while(!st.empty()&&preorder[i]>st.top()->val){
+                
+                parent = st.top();
+                st.pop();
+            }
+            
+            if(parent!=nullptr){
+                parent->right = node;
+            }
+            else if(!st.empty()){
+                st.top()->left = node;
+            }
+            st.push(node);
+        }
+        return root;
+    }
+};
