3aeb0bbaa76bfe4e0406a5b7752e887dd77b6e7b
Radhika Wadekar <60853138+radhika011@users.noreply.github.com>
2021-11-05 00:03:29 +0530

    Update Recursion w Arrays.cpp

--- CodingNinjas/Recursion/Recursion w Arrays.cpp
+++ CodingNinjas/Recursion/Recursion w Arrays.cpp
@@ -26,3 +26,25 @@ bool checkNumber(int input[], int size, int x) {
     return (input[0]==x)||rem;
 
 }
+
+
+int firstIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    if(size==0) return -1;
+    if(input[0]==x) return 0;
+    int next = firstIndex(input+1,size-1,x);
+    if(next==-1) return -1;
+    return 1+next;
+    
+}
+
+
+
+
+
+
+
