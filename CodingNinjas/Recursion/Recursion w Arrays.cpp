ad9964c75b7b125a1c4ee338110060e3ed8cabcc
Radhika Wadekar <60853138+radhika011@users.noreply.github.com>
2021-11-05 10:23:37 +0530

    Update Recursion w Arrays.cpp

--- CodingNinjas/Recursion/Recursion w Arrays.cpp
+++ CodingNinjas/Recursion/Recursion w Arrays.cpp
@@ -41,7 +41,25 @@ int firstIndex(int input[], int size, int x) {
     return 1+next;
     
 }
+//Simply interchanging positions of rec call and small calc
+int lastIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    
+    if(size<=0) return -1;
+    int next = lastIndex(input+1,size-1,x);
+     if(next==-1&&input[0]==x){
+        return 0;
+    }
+    if(next==-1) return -1;
+ 	return 1+next; 
+  
+    
 
+}
 
 
 
