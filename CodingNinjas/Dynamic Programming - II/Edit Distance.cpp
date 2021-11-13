ce41625034b615a8d31aaac89310f4137c642d97
Radhika Wadekar <60853138+radhika011@users.noreply.github.com>
2021-11-13 12:07:09 +0530

    Create Edit Distance.cpp

---
+++ CodingNinjas/Dynamic Programming - II/Edit Distance.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int getED(string s1,string s2){
+    int m = s1.size();
+    int n = s2.size();
+    vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+    for(int i =1;i<=m;i++){
+        dp[i][0] = i;
+    }
+    for(int j = 1;j<=n;j++){
+        dp[0][j] = j;
+    }
+    for(int i =1;i<=m;i++){
+        for(int j = 1;j<=n;j++){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = 1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+    }
+    return dp[m][n];
+    
+    
+    
+}
+
+
+int main(){
+    
+    // write your code here
+    int t;
+    cin>>t;
+    while(t--){
+        string s1,s2;
+        cin>>s1;
+        cin>>s2;
+        int ed = getED(s1,s2);
+        cout<<ed<<endl;
+        
+    }
+    return 0;
+}
