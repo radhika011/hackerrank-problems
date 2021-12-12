d97317cc92691fbce27e9563a5793a542eac4f81
Radhika Wadekar <radhikawadekar1@gmail.com>
2021-12-12 09:52:24 +0530

    Create Rings and Rods.cpp

---
+++ Rings and Rods.cpp
@@ -0,0 +1,34 @@
+
+/*
+There are n rings and each ring is either red, green, or blue. The rings are distributed across ten rods labeled from 0 to 9.
+
+You are given a string rings of length 2n that describes the n rings that are placed onto the rods. Every two characters in rings forms a color-position pair that is used to describe each ring where:
+
+The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
+The second character of the ith pair denotes the rod that the ith ring is placed on ('0' to '9').
+For example, "R3G2B1" describes n == 3 rings: a red ring placed onto the rod labeled 3, a green ring placed onto the rod labeled 2, and a blue ring placed onto the rod labeled 1.
+
+Return the number of rods that have all three colors of rings on them.
+
+
+*/
+
+
+
+class Solution {
+public:
+    int countPoints(string rings) {
+        unordered_map<int,set<char>> cache;
+        for(int i =0;i<rings.size();i+=2){
+            char color = rings[i];
+            int pos = rings[i+1]-'0';
+            cache[pos].insert(color);
+        }
+        int count = 0;
+        for(auto it = cache.begin();it!=cache.end();it++){
+            set<char> curr = it->second;
+            if(curr.size()==3) count++;
+        }
+        return count;
+    }
+};
