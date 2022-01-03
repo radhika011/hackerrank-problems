1a5808061ad64f4a255938cab0934004c2123b1f
Radhika Wadekar <radhikawadekar1@gmail.com>
2022-01-03 10:28:21 +0530

    Update Find the Town Judge.cpp

--- Find the Town Judge.cpp
+++ Find the Town Judge.cpp
@@ -48,3 +48,22 @@ public:
         return p_judge;
     }
 };
+//WTH WAS THAT? smh I could've just calculated the indegree and outdegree for each vertex
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> indegree(n+1,0);
+        vector<int> outdegree(n+1,0);
+        int n_edges = trust.size();
+        for(int i =0;i<n_edges;i++){
+            int from = trust[i][0];
+            int to = trust[i][1];
+            indegree[to]++;
+            outdegree[from]++;
+        }
+        for(int i =1;i<=n;i++){
+            if(indegree[i]==(n-1)&&outdegree[i]==0) return i;
+        }
+        return -1;
+    }
+};
