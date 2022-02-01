5b7530577bbc84df5e34a01703656f0efee58631
Radhika Wadekar <radhikawadekar1@gmail.com>
2022-02-01 09:34:09 +0530

    Update Best Time to Buy and Sell Stock I.cpp

--- Best Time to Buy and Sell Stock I.cpp
+++ Best Time to Buy and Sell Stock I.cpp
@@ -1,4 +1,4 @@
-//Inefficient
+//Inefficient - O(n^2)
 
 class Solution {
 public:
@@ -17,3 +17,20 @@ public:
 
 //For variation - 
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-:)-(In-case-if-interviewer-twists-the-input)
+
+//Efficient - O(n)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxDiff = 0;
+        int lower = prices[0];
+        for(int i =1;i<n;i++){
+           
+            int currDiff = prices[i]-lower;
+            maxDiff = max(maxDiff,currDiff);
+            lower = min(lower,prices[i]);
+        }
+        return maxDiff;
+    }
+};
