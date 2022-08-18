76d8ab9d8e3345f31bb14a30829d875a3b7f9ce6
Radhika Wadekar <radhikawadekar1@gmail.com>
2022-08-18 17:15:15 +0530

    Create 20. Largest Subarray with equal number of 0s and 1s.cpp

---
+++ Pepcoding/Arrays/20. Largest Subarray with equal number of 0s and 1s.cpp
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        
+        
+        unordered_map<int,int> first_occ;
+        int curr_sum = 0;
+        for(int i =0;i<n;i++){
+            int curr = nums[i];
+            if(curr==0){
+                curr_sum-=1;
+            }
+            else curr_sum+=1;
+            if(curr_sum==0){
+                ans = max(ans,i+1);
+                continue;
+            }
+            if(first_occ.find(curr_sum)!=first_occ.end()){
+                ans = max(ans,i-first_occ[curr_sum]);
+            }
+            else{
+                first_occ[curr_sum] = i;
+            }
+        }
+        
+        
+        
+        return ans;
+    }
+};
